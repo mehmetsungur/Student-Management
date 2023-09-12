49b9576b639785029a7122ed9f25fe5204108724
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-12 22:24:44 +0300

    update

---
+++ src/main/java/com/project/contactmessage/mapper/ContactMessageMapper.java
@@ -0,0 +1,13 @@
+package com.project.contactmessage.mapper;
+
+import com.project.contactmessage.dto.ContactMessageRequest;
+import com.project.contactmessage.entity.ContactMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContactMessageMapper {
+
+    public ContactMessage requestToContactMessage(ContactMessageRequest contactMessageRequest){
+        return null;
+    }
+}
