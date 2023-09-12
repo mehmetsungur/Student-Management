cc6c80ca01eb526907b6ae48831734113f0915cd
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-12 22:03:50 +0300

    update

--- src/main/java/com/project/contactmessage/controller/ContactMessageController.java
+++ src/main/java/com/project/contactmessage/controller/ContactMessageController.java
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
-
 @RestController
 @RequestMapping("/contactMessages")
 @RequiredArgsConstructor
 public class ContactMessageController {
-    private ContactMessageService contactMessageService;
-
-    @PostMapping("/save") //localhost:8080/contactMessages/save + POST + JSON
+    private final ContactMessageService contactMessageService;
+    @PostMapping("/save") // http://localhost:8080/contactMessages/save   + POST + JSON
     public ResponseMessage<ContactMessageResponse> save(@RequestBody @Valid ContactMessageRequest contactMessageRequest){
+
+        return null;
     }
 }
