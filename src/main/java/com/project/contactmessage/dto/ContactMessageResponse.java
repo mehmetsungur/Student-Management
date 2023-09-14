cbfea6ccccc958470b188e42c7077a918d1e930b
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-14 09:57:10 +0300

    update

--- src/main/java/com/project/contactmessage/dto/ContactMessageResponse.java
+++ src/main/java/com/project/contactmessage/dto/ContactMessageResponse.java
@@ -17,7 +17,6 @@ public class ContactMessageResponse {
     private String email;
     private String subject;
     private String message;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime dateTime;
 }
