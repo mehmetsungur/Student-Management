169e4dee04d9fe79e3d08cdd42d757184a3413bf
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-15 21:04:21 +0300

    update

--- src/main/java/com/project/entity/business/StudentInfo.java
+++ src/main/java/com/project/entity/business/StudentInfo.java
@@ -1,5 +1,25 @@
 package com.project.entity.business;
 
-public class StudentInfo {
+import com.project.entity.enums.Note;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class StudentInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer absentee;
+
+    private Double midtermExam;
+
+    private Double finalExam;
+
+    private Double examAverage;
+
+    private String infoNote;
+
+    private Note letterGrade;
 }
