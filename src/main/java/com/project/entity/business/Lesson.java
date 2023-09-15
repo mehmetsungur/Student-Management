c35af19a50e918dc8c5d470be5df661be629a557
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-15 21:19:21 +0300

    update

---
+++ src/main/java/com/project/entity/business/Lesson.java
@@ -0,0 +1,15 @@
+package com.project.entity.business;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Lesson {
+
+}
